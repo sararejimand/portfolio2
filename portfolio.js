2c986c4c902bbe1b1f146020921df4e36983690e
Sara <sararezhimand@gmail.com>
2025-12-17 12:26:17 +0100

    bug fix

--- portfolio.js
+++ portfolio.js
@@ -127,7 +127,7 @@ document.addEventListener('DOMContentLoaded', () => {
     gsap.registerPlugin(ScrollTrigger);
     
 
-fetch('./projects.json')
+fetch('./projects.json', { cache: "no-store" })
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
